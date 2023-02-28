1849797aa1fa572e97cd2c385a3e286212223005
yismaw17 <yismawmulaw@yahoo.com>
2023-02-27 23:32:14 -0800

    task binary

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: pointer to root node
+ * Return: nothing
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+		return;
+	}
+}
